767f821f6e80fc01bbb9cf2b19fbd58b0480bb4e
Yehezkiel-simbuang <simbuangyehezkiel@gmail.com>
2023-10-31 14:41:49 +0700

    delete package

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.json());
 
-
 app.use("/api/v1", combinedRouter);
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
