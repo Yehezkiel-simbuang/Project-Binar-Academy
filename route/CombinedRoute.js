6d97a2cdc0dead65588683e4daeb6fe3752b1ad5
Yehezkiel-simbuang <simbuangyehezkiel@gmail.com>
2023-10-31 14:28:52 +0700

    error route

--- route/CombinedRoute.js
+++ route/CombinedRoute.js
@@ -5,16 +5,16 @@ import { getTransaction, getTransactionDetails, postTransaction } from "../middl
 
 const combinedRouter = express.Router();
 
-combinedRouter.post("/users", getUser);
-combinedRouter.get("/users", postUser);
+combinedRouter.get("/users", getUser);
+combinedRouter.post("/create-user", postUser);
 combinedRouter.get("/users/:userid", getUserDetails);
 
-combinedRouter.post("/accounts", getAccount);
-combinedRouter.get("/accounts", postAccount);
+combinedRouter.get("/accounts", getAccount);
+combinedRouter.post("/create-accounts", postAccount);
 combinedRouter.get("/accounts/:accountid", getAccountDetails);
 
-combinedRouter.post("/transactions", getTransaction);
-combinedRouter.get("/transactions", postTransaction);
+combinedRouter.get("/transactions", getTransaction);
+combinedRouter.post("/create-transactions", postTransaction);
 combinedRouter.get("/transactions/:transactionid", getTransactionDetails);
 
 export default combinedRouter;
