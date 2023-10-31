dad54564b2a9f7c50ccaaaed78f57b9a04b269d6
Yehezkiel-simbuang <simbuangyehezkiel@gmail.com>
2023-10-31 14:25:31 +0700

    error index

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Connecting to port 3000.....")
-})
 
-app.use("api/v1", combinedRouter);
+app.use("/api/v1", combinedRouter);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
@@ -19,3 +16,7 @@ app.use((err, req, res, next) => {
         statusCode,
     });
 });
+
+app.listen(3000, () => {
+    console.log("Connecting to port 3000.....")
+})
